3a4d7d7a9f25267da2925cb496c1091b44933509
Zerokaze <erfu81856@gmail.com>
2025-07-19 13:55:29 +0800

    f

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    let  articles = [
+        {
+            title: "Article 1",
+            description: "This is the description of article 1",
+            link: "/article/1"
+        },
+        {
+            title: "Article 2",
+            description: "This is the description of article 2",
+            link: "/article/2"
+        },
+        {
+            title: "Article 3",
+            description: "This is the description of article 3",
+        },
+        {
+            title: "Article 4",
+            description: "This is the description of article 4",
+            link: "/article/4"
+        },
+
+    ]
+</script>
+
+
+<div class="min-h-screen flex flex-col items-center justify-start bg-gray-100 text-gray-800">
+{#each articles as article}
+    <div class="w-90/100  bg-amber-100">
+        <p>title:{article.title}</p>
+        <p>description:{article.description}</p>
+        <a href="{article.link}">Read more</a>
+    </div>
+{/each}
+
+</div>
