d8ca4edcf822ccb9ec1ea99edfa29c67945e0de1
Zerokaze <erfu81856@gmail.com>
2025-07-19 14:25:57 +0800

    f

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -13,24 +13,30 @@
         {
             title: "Article 3",
             description: "This is the description of article 3",
+            link: "/article/2"
+
         },
         {
             title: "Article 4",
             description: "This is the description of article 4",
             link: "/article/4"
         },
+        {
+            title: "Article 5",
+            description: "This is the description of article 5",
+            link: "/article/5"
+        }
 
     ]
 </script>
 
 
-<div class="min-h-screen flex flex-col items-center justify-start bg-gray-100 text-gray-800">
+<div class="min-h-screen columns-1  gap-4 md:columns-2 lg:columns-3 bg-gray-100 text-gray-800">
 {#each articles as article}
-    <div class="w-90/100  bg-amber-100">
+    <div class="w-90/100 break-inside-avoid mb-4  bg-slate-200 rounded-lg p-4">
         <p>title:{article.title}</p>
         <p>description:{article.description}</p>
         <a href="{article.link}">Read more</a>
     </div>
 {/each}
-
 </div>
